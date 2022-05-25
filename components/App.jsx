7caf22b7a41b05e2f77b8aba37e6b54c76d9ea3c
Ahljennetics <64159706+Ahljenn@users.noreply.github.com>
2022-05-25 04:01:23 +0000

    Added SeeMoreText

--- components/App.jsx
+++ components/App.jsx
@@ -17,22 +17,28 @@ function App() {
       </p>
 
       <SeeButton />
-      
 
 
 
 
-      
-      
-      <p>
-        Here's a quick look at some of the data on reservoirs from the <a href="https://cdec.water.ca.gov/index.html">California Data Exchange Center</a>, which consolidates climate and water data from multiple federal and state government agencies, and  electric utilities.  Select a month and year to see storage levels in the eleven largest in-state reservoirs.
-      </p>  
+
+
+
+
+
+
+
+
+
+
 
 
       
       <img src="https://cdn.theatlantic.com/thumbor/HYdYHLTb9lHl5ds-IB0URvpSut0=/900x583/media/img/photo/2014/09/dramatic-photos-of-californias-historic-drought/c01_53834006/original.jpg
 "/>
 Lake Oroville in the 2012-2014 drought. Image credit Justin Sullivan, from The Atlatic article Dramatic Photos of California's Historic Drought.
+
+      
     </main>
   );
 }
--- components/SeeButton.jsx
+++ components/SeeButton.jsx
@@ -1,5 +1,16 @@
 import React, { useState } from 'react';
 
+function SeeMoreText(props){
+  if (props.state == "See Less"){
+      return (        
+        <p>
+          Here's a quick look at some of the data on reservoirs from the <a href="https://cdec.water.ca.gov/index.html">California Data Exchange Center</a>, which consolidates climate and water data from multiple federal and state government agencies, and  electric utilities.  Select a month and year to see storage levels in the eleven largest in-state reservoirs.
+        </p>  
+    )  
+  }
+  return (<></>) 
+}
+
 function SeeButton() {
 
   const [buttonState, setButtonState] = useState("See More");
@@ -9,12 +20,17 @@ function SeeButton() {
   }
 
   return (
-    <button
-      className="see"
-      onClick={seeButtonHandler}
-    >
-      {buttonState}
-    </button>
+    <>
+      <button
+        className="see"
+        onClick={seeButtonHandler}>
+        {buttonState}
+      </button>
+
+      <div>
+        <SeeMoreText state={buttonState}/>
+      </div>
+    </>
   )
 }
 
